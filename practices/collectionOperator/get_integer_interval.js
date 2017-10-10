e582cdef908b1506d35da2f442bf4608ce66e736
OMGLoveling <742943632@qq.com>
2017-10-10 14:25:52 +0800

    finsh get_integer_interval(_2).js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
+    if(number_b>number_a){
+      return get_sort(number_a,number_b);
+    }
+    else if(number_b<=number_a){
+      return get_sort(number_b,number_a).reverse();
+    }
+}
+
+function get_sort(a,b) {
+  let collection = [];
+  for(let i=a;i<=b;i++){
+    collection.push(i);
+  }
+  return collection;
+
 }
 
 module.exports = get_integer_interval;
--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,36 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  if(number_b>number_a){
+    return get_sort(number_a,number_b);
+  }
+  else if(number_b<number_a){
+    return get_sort(number_b,number_a).reverse();
+  }
+  else if(number_a=number_b){
+    if(number_b%2===1)
+    {
+      return get_sort(number_a,number_b);
+    }
+    else if(number_b%2===0){
+      return get_sort_1(number_a,number_b);
+    }
+  }
+
+}
+function get_sort(a,b) {
+  let collection = [];
+  for(let i=a+1;i<=b;i+=2){
+    collection.push(i);
+  }
+  return collection;
+}
+function get_sort_1(a,b) {
+  let collection_1=[];
+  for(let i=a;i<=b;i++) {
+    collection_1.push(i);
+  }
+  return collection_1;
 }
 
 module.exports = get_integer_interval_2;
