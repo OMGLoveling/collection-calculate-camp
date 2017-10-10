dae605b02a4f312d5ba4d32ed13b29bff30c83a9
OMGLoveling <742943632@qq.com>
2017-10-10 14:45:43 +0800

    finish collect_last_element

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,16 @@
 'use strict';
 
-function collect_all_even(collection) {
-  //在这里写入代码
+function collect_all_even(collection_a) {
+  let  collection_b=[];
+  let  j=0;
+  collection_a.forEach( i=>{
+      if(i%2===0)
+      {
+       collection_b[j]=i;
+       j++;
+      }
+  })
+  return collection_b;
 }
 
 module.exports = collect_all_even;
--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+  let k;
+  collection.forEach(function (elem,i){
+      if(i===collection.length-1)
+      {
+          k = collection[i];
+      }
+
+  })
+  return k;
 }
 
 module.exports = collect_last_element;
